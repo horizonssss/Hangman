06877f06bfe8ce9961551437568bf2f0d6c3987a
horizonssss <brad4125@gmail.com>
2024-04-02 19:02:26 +1100

    Added some functions, halfway there. That's enough for tonight!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 def get_random_word_from_word_list():
     """
@@ -22,4 +23,104 @@ def get_random_word_from_word_list():
         print(f"An error has occurred: {e}")
         return None
 
-print(get_random_word_from_word_list())
+def define_length_of_word(word):
+    """
+    Defines the length of a given input word, returning underscores
+    for the length of a word with spaces in between.
+
+    Parameters:
+        word (str or None): The word to be processed or None.
+
+    Returns:
+        str: Underscore and space per letter input.
+    """
+    # Check word is not None
+    if word is None:
+        return "No word provided, please provide a word."
+    # Initialise the underscores variable
+    underscores = ""
+    # Add an underscore & space for each letter in the word
+    underscores = len(word) * "_ "
+
+    return underscores
+
+def draw_hangman(chances):
+    """
+    Prints out the hangman picture, depending on remaining chances.
+
+    Parameters:
+        integer (int or None): The number to be processed or None.
+    """
+    if chances == 6:
+        print("________ ")
+        print("| | ")
+        print("| ")
+        print("| ")
+        print("| ")
+        print("| ")
+    elif chances == 5:
+        print("________ ")
+        print("| | ")
+        print("| 0 ")
+        print("| ")
+        print("| ")
+        print("| ")
+    elif chances == 4:
+        print("________ ")
+        print("| | ")
+        print("| 0 ")
+        print("| / ")
+        print("| ")
+        print("| ")
+    elif chances == 3:
+        print("________ ")
+        print("| | ")
+        print("| 0 ")
+        print("| /| ")
+        print("| ")
+        print("| ")
+    elif chances == 2:
+        print("________ ")
+        print("| | ")
+        print("| 0 ")
+        print("| /|\ ")
+        print("| ")
+        print("| ")
+    elif chances == 1:
+        print("________ ")
+        print("| | ")
+        print("| 0 ")
+        print("| /|\ ")
+        print("| / ")
+        print("| ")
+    elif chances == 0:
+        print("________ ")
+        print("| | ")
+        print("| 0 ")
+        print("| /|\ ")
+        print("| / \ ")
+        print("| ")
+
+def remaining_letters(guessed_letters):
+    """
+    Prints the alphabet with letters that have been guessed crossed out.
+
+    Parameters:
+    guessed_letters (set or list): The letters that have been guessed so far.
+    """
+    if guessed_letters is None:
+        return None
+    
+    guessed_letters_before_strike = ""
+    for letter in guessed_letters:
+        if letter in string.ascii_lowercase:
+            guessed_letters_before_strike += letter + '\u0336'
+    print(guessed_letters_before_strike)
+
+
+
+
+
+# Testing
+print(define_length_of_word(get_random_word_from_word_list()))
+remaining_letters("abcx")
